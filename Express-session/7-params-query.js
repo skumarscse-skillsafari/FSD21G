6993745af40e8a273bcc74f21b6ba8becb75144e
Sathishkumar <skumarscse@gmail.com>
2022-06-08 20:03:30 +0530

    query and params

--- Express-session/7-params-query.js
+++ Express-session/7-params-query.js
@@ -16,9 +16,16 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:productID', (req, res) => {
-    // console.log(req.params);
+    console.log(req.params);
     const { productID } = req.params;
+    // const req = {
+    //     params: {
+    //         productID: '3'
+    //     }
+    // }
+    // req.params.productID
     const singleProduct = products.find((product) => product.id === Number(productID));
+    console.log(singleProduct);
     if(!singleProduct) {
         res.status(404).send('<h2>Product Not Found!!!</h2>');
     }
@@ -28,6 +35,27 @@ app.get('/api/products/:productID', (req, res) => {
 app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
     console.log(req);
     console.log(req.params);
+});
+
+app.get('/api/v1/products/query', (req, res) => {
+    // console.log(req);
+    // console.log(req.query);
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+    // console.log(sortedProducts);
+    if(search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.title.startsWith(search);
+        })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1) {
+        // return res.status(200).send('No products matched...');
+        return res.status(200).json({ success: true, data: [] });
+    }
+    res.status(200).json(sortedProducts);
 })
 
 app.listen(5000, () => {
