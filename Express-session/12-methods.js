ba94959df04d4c26b81c19e77049a8a804014c64
Sathishkumar <skumarscse@gmail.com>
2022-06-15 10:30:55 +0530

    testing put, post, delete in postman

--- Express-session/12-methods.js
+++ Express-session/12-methods.js
@@ -8,7 +8,8 @@ app.use(express.static('./methods-public'));
 // parse form data: urlencoded middleware
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/users', (req, res) => {
+//Getting all the Users
+app.get('/api/users', (req, res) => { //http://localhost:5000/api/users
     res.status(200).json({ success: true, data: users});
 });
 
@@ -17,12 +18,65 @@ app.post('/login', (req, res) => {
     // console.log(req.body);
     const {name} = req.body;
     // console.log(name);
-    if (!name) {
-        return res.status(400).json({success: false, msg: 'Please enter a value in name field'});
+    if (name) {
+        return res.status(200).send(`Welcome user: ${name}`);
     }
-    res.status(201).json({success: true, user: name});
+    res.status(401).send('Please provide username');
 })
 
+// Creating single User
+app.post('/api/user', (req, res) => {
+    const {name} = req.body;
+    if(!name) {
+        return res.status(400).json({success: false, msg: 'Please enter username'});
+    }
+    res.status(201).json({success: true, data: name});
+});
+
+// Create a new user
+app.post('/api/postman/user', (req, res) => {
+    const {name} = req.body;
+    if(!name) {
+        return res.status(400).json({success: false, msg: 'Please enter username'});
+    }
+    res.status(201).json({success: true, data: name});
+});
+
+// Get a Single User
+app.get('/api/user/:id', (req, res) => { // http://localhost:5000/api/user/1
+    const {id} = req.params;
+    const singleUser = users.find((user) => user.id === Number(id));
+    res.status(200).json({success: true, data: singleUser});
+});
+
+// Update a User
+app.put('/api/user/:id', (req, res) => { // http://localhost:5000/api/user/1
+    const {id} = req.params;
+    const {name} = req.body;
+    const user = users.find((user) => user.id === Number(id));
+    if (!user) {
+        return res.status(404).json({success: false, msg: `No User with id: ${id}`});
+    }
+    const newUser = users.map((user) => {
+        if(user.id === Number(id)) {
+            user.name = name;
+        }
+        return user;
+    })
+    res.status(200).json({success: true, data: newUser});
+});
+
+// Delete a User
+app.delete('/api/user/:id', (req, res) => {
+    const {id} = req.params;
+    const user = users.find((user) => user.id === Number(id));
+    if (!user) {
+        return res.status(404).json({success: false, msg: `No User with id: ${id}`});
+    }
+    const newUsers = users.filter((user) => {
+        user.id !== Number(id);
+    })
+})
 
 app.listen(5000, () => {
     console.log('Server is running in: http://localhost:5000');
