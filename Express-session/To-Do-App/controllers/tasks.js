2a36e689891b03290f9506f6a8a448b33303a2b7
Sathishkumar <skumarscse@gmail.com>
2022-06-21 19:57:56 +0530

    read-update-delete-from-mongodb

--- Express-session/To-Do-App/controllers/tasks.js
+++ Express-session/To-Do-App/controllers/tasks.js
@@ -1,22 +1,34 @@
-import Task from '../models/Task.js';
+import Task from "../models/Task.js";
 
 export const createTask = async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-}
+  console.log(req.body); // { "name": :EduKeys, "Completed": true}
+  const task = await Task.create(req.body); // { name: 'EduKeys', completed: true }
+  res.status(201).json({ task }); // { task: { name: 'EduKeys', completed: true, _id: , _v: 0 }}
+};
 
-export const getAllTasks = (req, res) => {
-    res.send('Get All Tasks...');
-}
+export const getAllTasks = async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+};
 
-export const getTask = (req, res) => {
-    res.send('Get Task...');
-}
+export const getTask = async (req, res) => {
+  // console.log(req.params);
+  const { id: taskID } = req.params;
+  const singleTask = await Task.findOne({ _id: taskID });
+  res.status(200).json({ singleTask });
+};
 
-export const updateTask = (req, res) => {
-    res.send('Update Task...');
-}
+export const updateTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  const updatedTask = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).send({ updatedTask });
+};
 
-export const deleteTask = (req, res) => {
-    res.send('Delete Task...');
-}
+export const deleteTask = async (req, res) => {
+  const { id: taskID } = req.params;
+  const deletedTask = await Task.findOneAndDelete({ _id: taskID });
+  res.status(200).send({ deletedTask });
+};
